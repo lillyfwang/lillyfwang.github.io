a906bc3ea436e84011c5b9f964c2906015b14770
lillyfwang <lillyfwang@yahoo.com>
2014-02-01 18:34:38 -0500

    Completed template for the homepage of my website

--- script.js
+++ script.js
@@ -1,5 +1,38 @@
-$(document).ready(function(){
-	
-	//Code Here, prep for new website design
+$(document).ready(function() {
+
+	$('div').hover(
+		function()
+		{
+			$(this).css("z-index", "100");
+			$(this).css("border", "dotted 2px #FFFFFF");
+			$(this).fadeTo(1);
+		},
+		function() {
+      		 $(this).css("z-index", "-100");
+      		 $(this).css("border", "none");
+      		 $(this).fadeTo(.3);
+  		 });
+
+	$('.updown').hover(
+		function()
+		{
+			$(this).animate({height: "+=15px"})
+		},
+		function()
+		{
+			$(this).animate({height: "-=15px"})
+		});
+
+
+	$('.leftright').hover(
+		function()
+		{
+			$(this).animate({width: "+=15px"})
+		},
+		function()
+		{
+			$(this).animate({width: "-=15px"})
+		});
+
 
 });

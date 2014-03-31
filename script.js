fde6b8772877a00832c1e596c390e61bef6bdfe1
lillyfwang <lillyfwang@yahoo.com>
2014-03-31 10:52:36 -0400

    fixed text so it is easier to read, resolves #3

--- script.js
+++ script.js
@@ -31,7 +31,7 @@ $(document).ready(function() {
 		function()
 		{$(this).animate({left: "+=2%"})});
 
-	//ABOUT PAGE TOGGLE
+	//ABOUT PAGE TOGGLES
 	var aboutOpen=false;
 	$('#about').click(function(){
 		aboutOpen=!aboutOpen;
@@ -39,16 +39,18 @@ $(document).ready(function() {
 			$(this).animate({bottom: "0%", opacity: 1, backgroundColor: "#FFFCF5", color: "#2E2511"}, 800);
 			$(this).css("z-index", "10");
 			$('#about-inside').css("border", "solid 5px black");
+			$('#about-inside').css("opacity","1");
 		}
 		else
 		{
 			$(this).animate({bottom: "78%",opacity: .4, backgroundColor: "rgb(0, 126, 237)", color: "#FFFCF5"}, 800);
 			$(this).css("z-index", "1");
 			$('#about-inside').css("border", "none");
+			$('#about-inside').css("opacity",".4");
 		}
 	});
 
-	//CONTACT PAGE TOGGLE
+	//CONTACT PAGE TOGGLES
 	var contactOpen=false;
 	$('#contact').click(function(){
 		contactOpen=!contactOpen;
@@ -56,16 +58,18 @@ $(document).ready(function() {
 			$(this).animate({right: "0%", opacity: 1, backgroundColor: "#FFFCF5", color: "#2E2511"}, 800);
 			$(this).css("z-index", "10");
 			$('#contact-inside').css("border", "solid 5px black");
+			$('#contact-inside').css("opacity","1");
 		}
 		else
 		{
 			$(this).animate({right: "72%",opacity: .4, backgroundColor: "#C68CFF", color: "#FFFCF5"}, 800);
 			$(this).css("z-index", "2");
 			$('#contact-inside').css("border", "none");
+			$('#contact-inside').css("opacity",".4");
 		}
 	});
 
-	//GITHUB PAGE TOGGLE
+	//GITHUB PAGE TOGGLES
 	var ghOpen=false;
 	$('#github').click(function(){
 		ghOpen=!ghOpen;
@@ -73,16 +77,18 @@ $(document).ready(function() {
 			$(this).animate({top: "0%", opacity: 1, backgroundColor: "#FFFCF5", color: "#2E2511"}, 800);
 			$(this).css("z-index", "10");
 			$('#github-inside').css("border", "solid 5px black");
+			$('#github-inside').css("opacity","1");
 		}
 		else
 		{
 			$(this).animate({top: "84%",opacity: .4, backgroundColor: "rgb(0, 198, 237)", color: "#FFFCF5"}, 800);
 			$(this).css("z-index", "3");
 			$('#github-inside').css("border", "none");
+			$('#github-inside').css("opacity",".4");
 		}
 	});
 
-	//RESUME PAGE TOGGLE
+	//RESUME PAGE TOGGLES
 	var resumeOpen=false;
 	$('#resume').click(function(){
 		resumeOpen=!resumeOpen;
@@ -90,12 +96,14 @@ $(document).ready(function() {
 			$(this).animate({left: "0%", opacity: 1, backgroundColor: "#FFFCF5", color: "#2E2511"}, 800);
 			$(this).css("z-index", "10");
 			$('#resume-inside').css("border", "solid 5px black");
+			$('#resume-inside').css("opacity","1");
 		}
 		else
 		{
 			$(this).animate({left: "89%",opacity: .4, backgroundColor: "#00E384", color: "#FFFCF5"}, 800);
 			$(this).css("z-index", "4");
 			$('#resume-inside').css("border", "none");
+			$('#resume-inside').css("opacity",".4");
 		}
 	});
 

f31ab1ef5fe972b7ea4d2e26b7cd7ffad6656475
lillyfwang <lillyfwang@yahoo.com>
2014-04-04 00:12:48 -0400

    Added Google Analytics tracking

--- script.js
+++ script.js
@@ -107,4 +107,13 @@ $(document).ready(function() {
 		}
 	});
 
+
+//Google Analytics Tracking
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-49697905-1', 'lillyfwang.com');
+  ga('send', 'pageview');
 });

068720e7f26d1289a97eaa0611273dc50dac718a
lillyfwang <lillyfwang@yahoo.com>
2014-09-18 21:47:08 -0400

    updated formatting of website, removed skelJS

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,34 +1,3 @@
-window._skel_config = {
-  prefix: "assets/css/style",
-  resetCSS: true,
-  boxModel: "border",
-  grid: { gutters: 30 },
-  breakpoints: {
-    wide: { range: '1200-', containers: 1140, grid: { gutters: 50 } },
-    narrow: { range: '960-1199', containers: 960 },
-    narrower: { range: '570-959', containers: 570 },
-    mobile: { range: '-569', containers: 'fluid', lockViewport: true, grid: { collapse: true, gutters: 20 } }
-  }
-};
-
-window._skel_panels_config = {
-  panels: {
-    navPanel: {
-      breakpoints: 'mobile',
-      position: 'top',
-      size: 400,
-      html: '<div data-action="copyHTML" data-args="navBar"></div>'
-    }
-  },
-  overlays: {
-    navBar: {
-      position: 'top-center',
-      width: '100%',
-      height:60,
-    }
-  }
-};
-
 $(document).ready(function(){
   $('a[href*=#]:not([href=#])').click(function() {
     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') 
@@ -49,5 +18,9 @@ $(document).ready(function(){
 
   $(function() {
     $( "#tabs" ).tabs();
+  });
+
+  $(function() {
+    $('#resume-info').css("height", $('#design').height());
   });
 });
